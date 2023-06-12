d36f2cf89741dcaa2bb92b5e2ece9b08c10755a3
kanghohyeong <hobro.kang@gmail.com>
2023-06-12 12:20:31 +0900

    no console log in production

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {GAME_STATUS} from "./model/enums";
 import Main from "./pages/Main";
 import Loading from "./pages/Loading";
@@ -10,6 +10,15 @@ function App() {
 
     const [gameStatus, setGameStatus] = useState(GAME_STATUS.INIT);
     const [progress, setProgress] = useState(0);
+
+    useEffect(() => {
+        if (process.env.NODE_ENV === 'production') {
+            console.log = function no_console() {
+            };
+            console.warn = function no_console() {
+            };
+        }
+    }, []);
 
     return (
         <div className="App">
