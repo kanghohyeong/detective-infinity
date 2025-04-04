fbd6d708f7877b6c74ccfb2c5f1409aa4382f307
kanghohyeong <hobro.kang@gmail.com>
2025-04-04 17:57:56 +0900

    add watson character img

--- src/components/Watson.js
+++ src/components/Watson.js
@@ -1,47 +1,30 @@
 import React from 'react';
-import {useState} from "react";
+import { useState } from "react";
 import WatsonSurvey from "./WatsonSurvey";
 import useScenarioStore from "../store/scenarioStore";
 import useGameStore from "../store/gameStore";
-import {useChatGpt} from "../hooks/useChatGpt";
-import {getWatsonSystemMessage} from "../prompt/prompt";
+import { useChatGpt } from "../hooks/useChatGpt";
+import { getWatsonSystemMessage } from "../prompt/prompt";
 import styles from '../styles/components/Watson.module.css';
 
 const Watson = () => {
     const [onSurvey, setOnSurvey] = useState(false);
     const { apiKey, watsonChatHistory, updateWatsonChatHistory } = useGameStore();
     const scenario = useScenarioStore((state) => state.scenario);
-    const {count, chat} = useChatGpt(apiKey, getWatsonSystemMessage(scenario));
+    const { count, chat } = useChatGpt(apiKey, getWatsonSystemMessage(scenario));
 
     const messages = watsonChatHistory;
     const setMessages = updateWatsonChatHistory;
 
     return (
         <div className={styles.container}>
-            <pre className={styles.ascii} onClick={() => setOnSurvey(true)}>
-                <code>{`
-  ___
-/     \\
-/_______\\
-/      \\
-/        \\
-|  |    |  |
-\\  \\__/  /
-\\______/
-\\\\||//
-\\\\||//
-.----\\\\||//----.
-(     /\\\\  /\\\\     )
-(    /  \\\\/  \\\\    )
-(   /    ||    \\\\   )
-(  /     ||     \\\\  )
-( /      ||      \\\\ )
-(<       ||       >)
-\\\\      ||      /
-\\\\____/\\\\____/
-`}
-                </code>
-            </pre>
+            <img
+                src={`${process.env.PUBLIC_URL}/assets/detective-infinity-watson.png`}
+                alt='watson'
+                onClick={()=>setOnSurvey(true)}
+                className={styles.character}
+            />
+           
             <h3 className={styles.title}>What shall we investigate, detective?</h3>
             <p className={styles.description}>crime scene investigation, surrounding investigation, etc..</p>
             {onSurvey && (
